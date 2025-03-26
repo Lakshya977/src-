af5cfbec3ad94e3e0c0b3d06b6216ff645cbf754
Lakshya <Lakshya8998@gmail.com>
2025-03-27 01:56:08 +0530

    jejej

---
+++ utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({ 
+    cloud_name: 'dp57vjwyl', 
+    api_key: '546586713529885', 
+    api_secret: process.env.CLOUDINARY_KEY
+});
+
+export const uploadToCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) {
+            throw new Error("No file path provided!");
+        }
+
+        // Upload file to Cloudinary
+        const result = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto", 
+        });
+         console.log("file is uplaoded",result.url);
+        return result
+    } catch (error) {
+        fs.unlinkSync(localFilePath)// remove shit
+        console.error("Cloudinary Upload Error:", error);
+        throw error;
+    }
+
+};
+ export  default {uploadOnCloudinary}
